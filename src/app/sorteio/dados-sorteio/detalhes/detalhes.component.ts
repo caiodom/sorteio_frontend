4d273fcf911770500e238e53d9cab615d003208b
caio_dom <caio_henrrique@hotmail.com>
2024-03-05 11:13:28 -0300

    participante sorteio completo

--- src/app/sorteio/dados-sorteio/detalhes/detalhes.component.ts
+++ src/app/sorteio/dados-sorteio/detalhes/detalhes.component.ts
@@ -1,18 +1,21 @@
 import { ActivatedRoute } from '@angular/router';
 import { DadosSorteio } from './../models/dados-sorteio';
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/utils/shared-variables';
+import { FormBaseGlobalComponent } from 'src/app/base-components/form-base-global.component';
 
 @Component({
   selector: 'app-detalhes',
   templateUrl: './detalhes.component.html',
   styleUrls: ['./detalhes.component.css']
 })
-export class DetalhesComponent {
+export class DetalhesComponent extends FormBaseGlobalComponent {
 
   dadosSorteio:DadosSorteio= new DadosSorteio();
 
-  constructor(private route: ActivatedRoute) {
-
+  constructor(private route: ActivatedRoute,
+    protected override sharedService: SharedService) {
+    super(sharedService);
     this.dadosSorteio=this.route.snapshot.data['dadosSorteio'];
 
   }
--- src/app/sorteio/participante-sorteio/detalhes/detalhes.component.ts
+++ src/app/sorteio/participante-sorteio/detalhes/detalhes.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormBaseGlobalComponent } from 'src/app/base-components/form-base-global.component';
+import { SharedService } from 'src/app/utils/shared-variables';
+import { ParticipanteSorteio } from '../models/participante-sorteio';
 
 @Component({
   selector: 'app-detalhes',
   templateUrl: './detalhes.component.html',
   styleUrls: ['./detalhes.component.css']
 })
-export class DetalhesComponent implements OnInit {
+export class DetalhesComponent extends FormBaseGlobalComponent implements OnInit {
 
-  constructor() { }
+
+  participanteSorteio:ParticipanteSorteio= new ParticipanteSorteio();
+
+  constructor(private route: ActivatedRoute,
+    protected override sharedService: SharedService) {
+
+    super(sharedService);
+
+    this.participanteSorteio=this.route.snapshot.data['participanteSorteio'];
+  }
 
   ngOnInit(): void {
   }
