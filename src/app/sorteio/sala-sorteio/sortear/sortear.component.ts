048956f91aa52bafe207abee96f9ccf6de2f83fd
caio_dom <caio_henrrique@hotmail.com>
2024-03-06 14:50:44 -0300

    validaçao envio

--- src/app/sorteio/sala-sorteio/sortear/sortear.component.ts
+++ src/app/sorteio/sala-sorteio/sortear/sortear.component.ts
@@ -1,69 +1,79 @@
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { Component, ElementRef, Input, OnInit, ViewChildren } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChildren,
+} from '@angular/core';
 import { DadosSorteio } from '../../dados-sorteio/models/dados-sorteio';
-import { FormBuilder, FormControlName, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControlName,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { FormBaseComponent } from 'src/app/base-components/form-base.component';
 import { DadosSorteioService } from '../../dados-sorteio/services/dados-sorteio.service';
 
 @Component({
   selector: 'app-sortear',
   templateUrl: './sortear.component.html',
-  styleUrls: ['./sortear.component.css']
+  styleUrls: ['./sortear.component.css'],
 })
 export class SortearComponent extends FormBaseComponent implements OnInit {
+  @ViewChildren(FormControlName, { read: ElementRef })
+  formInputElements: ElementRef[];
 
-
-  @ViewChildren(FormControlName, { read: ElementRef }) formInputElements: ElementRef[];
-
-
-
-  @Input() dadosSorteio:DadosSorteio[];
+  @Input() dadosSorteio: DadosSorteio[];
   @Input() title: string;
   @Input() message: string;
-  errorMessage:string;
+  errorMessage: string;
 
+  dadosSorteioModal: DadosSorteio[];
+  sorteioForm: FormGroup;
+  idSorteio: string;
 
-  dadosSorteioModal:DadosSorteio[];
-  sorteioForm:FormGroup;
-  idSorteio:string;
-
-  constructor(private fb: FormBuilder,
-             private activeModal: NgbActiveModal) {
-
+  constructor(private fb: FormBuilder, private activeModal: NgbActiveModal) {
     super();
 
-    this.validationMessages={
-      idSorteio:{
-        required:'Informe o sorteio'
-      }
-    }
+    this.validationMessages = {
+      idSorteio: {
+        required: 'Informe o sorteio',
+      },
+    };
 
     super.configurarMensagensValidacaoBase(this.validationMessages);
-   }
+  }
 
   ngOnInit(): void {
-
-    this.sorteioForm=this.fb.group({
-      idSorteio:['',[Validators.required]]
+    this.sorteioForm = this.fb.group({
+      idSorteio: ['', [Validators.required]],
     });
 
-    this.dadosSorteioModal=this.dadosSorteio;
+    this.dadosSorteioModal = this.dadosSorteio;
   }
 
-  ngAfterViewInit(){
-    super.configurarValidacaoFormularioBase(this.formInputElements, this.sorteioForm);
+  ngAfterViewInit() {
+    super.configurarValidacaoFormularioBase(
+      this.formInputElements,
+      this.sorteioForm
+    );
   }
 
-  enviaSorteio(){
-
-    this.idSorteio=Object.assign({},this.idSorteio,this.sorteioForm.value);
-    this.activeModal.close(this.idSorteio);
-
+  enviaSorteio() {
+    if (this.sorteioForm.dirty && this.sorteioForm) {
+      this.idSorteio = Object.assign(
+        {},
+        this.idSorteio,
+        this.sorteioForm.value
+      );
+      this.activeModal.close(this.idSorteio);
+    }
 
   }
 
   public dismiss() {
     this.activeModal.dismiss();
   }
-
 }
